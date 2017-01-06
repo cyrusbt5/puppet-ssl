e08aaf340a37525a0b472f0a7e70cb4328ca5cf0
Robert Gurtler <robert.gurtler@anu.edu.au>
2017-01-06 18:58:46 +1100

    moved package out of case reduce dublication

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,27 +2,27 @@
 # Sets up some params based on OSFamily for the remainder of the module to make
 # use of
 class ssl::params {
+
+  $package = 'openssl'
+
   case $::osfamily {
     'RedHat': {
-      $package = 'openssl'
       $crt_dir = '/etc/pki/tls/certs'
       $key_dir = '/etc/pki/tls/private'
     }
     'Debian': {
-      $package = 'openssl'
       $crt_dir = '/etc/ssl/certs'
       $key_dir = '/etc/ssl/private'
     }
     'Archlinux': {
-      $package = 'openssl'
       $crt_dir = '/etc/ssl/certs'
       $key_dir = '/etc/ssl/private'
     }
     default: {
       # default to RedHat-style defaults
-      $package = 'openssl'
       $crt_dir = '/etc/pki/tls/certs'
       $key_dir = '/etc/pki/tls/private'
     }
   }
 }
+
